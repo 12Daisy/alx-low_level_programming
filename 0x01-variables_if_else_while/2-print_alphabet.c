f3f23421cdf253d88b8d2cb1b61ffb54ada65cec
12Daisy <keiemiledaisy5@gmail.com>
2023-04-21 13:03:57 -0700

    comments style

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,13 +11,13 @@
 int main(void) {
     char letter = 'a';
 
-    // print each letter of the alphabet using putchar
+    /* print each letter of the alphabet using putchar */
     while (letter <= 'z') {
         putchar(letter);
         letter++;
     }
 
-    // print a newline character at the end
+    /* print a newline character at the end */
     putchar('\n');
 
     return 0;
