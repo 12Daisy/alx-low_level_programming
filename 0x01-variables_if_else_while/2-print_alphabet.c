3d6e99220f1038691891114e91f3b6e1e9ea26e0
12Daisy <keiemiledaisy5@gmail.com>
2023-04-21 13:18:26 -0700

    betty style

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-    char letter = 'a';
+	char low;
 
-    /* print each letter of the alphabet using putchar */
-    while (letter <= 'z') {
-        putchar(letter);
-        letter++;
-    }
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+		putchar('\n');
 
-    /* print a newline character at the end */
-    putchar('\n');
-
-    return 0;
+	return (0);
 }
 
