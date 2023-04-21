eb5f4034bcafeddf0f34bcc0ec9e43745c62dcc7
12Daisy <keiemiledaisy5@gmail.com>
2023-04-21 13:35:17 -0700

    corrections

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+
 int main(void)
 {
     char low;
@@ -14,7 +8,6 @@ int main(void)
     {
         putchar(low);
     }
-
     putchar('\n');
 
     return 0;
