cb6a6be082489893431ce719b951a9e84600e925
12Daisy <keiemiledaisy5@gmail.com>
2023-04-21 12:57:10 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void) {
+    char letter = 'a';
+
+    // print each letter of the alphabet using putchar
+    while (letter <= 'z') {
+        putchar(letter);
+        letter++;
+    }
+
+    // print a newline character at the end
+    putchar('\n');
+
+    return 0;
+}
+
